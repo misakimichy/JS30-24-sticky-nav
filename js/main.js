cc44bb6b8052abab73007757db834b69faaacaa0
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-28 15:26:32 -0700

    Add fixNav function to add and remove .fixed-nav class to body

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function(){
+    const nav = document.querySelector('#main');
+    const topNav = nav.offsetTop;
+
+    const fixNav = () => {
+        if (window.scrollY >= topNav) {
+            document.body.style.paddingTop = `${nav.offsetHeight}px`;
+            document.body.classList.add('fixed-nav');
+        } else {
+            document.body.style.paddingTop = 0;
+            document.body.classList.remove('fixed-nav');
+        }
+    };
+    window.addEventListener('scroll', fixNav);
+}());
